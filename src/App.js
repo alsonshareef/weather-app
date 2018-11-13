2a4f3e3e94624212b00a028539bfd0460d68600f
Alson Shareef <alson.shareef@gmail.com>
2018-11-13 20:55:40 +0800

    Passed response data as props to Weather component and displayed weather data on UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Weather from './components/Weather';
 const API_KEY = '771a87d97e2ea9428a4f954571bba7bd';
 
 export default class App extends Component {
+  // Initial state
   state = {
+    latitude: null,
+    longitude: null,
+    coordinates: null,
     temperature: null,
     city: null,
     country: null,
@@ -16,21 +20,27 @@ export default class App extends Component {
   }
 
   getData = async (e) => {
-    e.preventDefault(); // Prevents full page refresh so data successfully appears
+    // Prevents full page refresh so data successfully appears
+    e.preventDefault();
+
+    // Set input values to variables
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     
+    // Filter fetch request URL based on input values and logs response data
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`).then(response => response.json());
     console.log(data);
 
+    // Change initial state props to the response data
     this.setState({
+      latitude: data.coord.lat,
+      longitude: data.coord.lon,
       temperature: data.main.temp,
       city: data.name,
       country: data.sys.country,
       humidity: data.main.humidity,
       description: data.weather[0].description
     })
-
   }
 
   render() {
@@ -38,7 +48,16 @@ export default class App extends Component {
       <div>
         <Titles />
         <Form getData={this.getData}/>
-        <Weather />
+        <Weather 
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
+          coordinates={this.state.coordinates}
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+        />
       </div>
     )
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ export default class Weather extends Component {
   render() {
     return (
       <div>
-        
+        <div>Location: {this.props.city}, {this.props.country}</div>
+        <div>Latitude: {this.props.latitude}  Longitude: {this.props.longitude}</div>
+        <div>Temperature: {this.props.temperature}</div>
+        <div>Humidity: {this.props.humidity}</div>
+        <div>Description: {this.props.description}</div>
       </div>
     )
   }
