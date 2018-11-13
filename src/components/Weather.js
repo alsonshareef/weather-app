55eab8c29430910e39a37c6d62bbcc66893c2cd2
Alson Shareef <alson.shareef@gmail.com>
2018-11-13 21:44:36 +0800

    Conditional rendering in Weather component. If true, then render data and if false then nothing will display

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,12 @@ export default class Weather extends Component {
   render() {
     return (
       <div>
-        <p>Location: {this.props.city}, {this.props.country}</p>
-        <p>Latitude: {this.props.latitude}  Longitude: {this.props.longitude}</p>
-        <p>Temperature: {this.props.temperature}</p>
-        <p>Humidity: {this.props.humidity}</p>
-        <p>Description: {this.props.description}</p>
-        <h2>{this.props.error}</h2>
+        {this.props.city && this.props.country ? <p>Location: {this.props.city}, {this.props.country}</p> : null}
+        {this.props.latitude && this.props.longitude ? <p>Latitude: {this.props.latitude}  Longitude: {this.props.longitude}</p> : null}
+        {this.props.temperature ? <p>Temperature: {this.props.temperature}</p> : null}
+        {this.props.humidity ? <p>Humidity: {this.props.humidity}</p> : null}
+        {this.props.description ? <p>Description: {this.props.description}</p> : null}
+        {this.props.error ? <h2>{this.props.error}</h2> : null}
       </div>
     )
   }
